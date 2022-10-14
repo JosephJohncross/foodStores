b52cd780de3efad0fb0706fe46a6f82b31384a18
Joseph Ibochi <josephibochi@gmail.com>
2022-10-14 04:20:14 +0100

    signal for user, custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,7 @@
 from email.policy import default
-from enum import auto
-from random import choices
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
 class UserManager(BaseUserManager):
@@ -85,3 +84,22 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="users/profile_pictures", blank=True, null=True)
+    cover_photo = models.ImageField(upload_to="users/cover_photos", blank=True, null=True)
+    address_line_1 = models.CharField(max_length=50, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=15, blank=True, null=True)
+    state = models.CharField(max_length=15, blank=True, null=True)
+    city = models.CharField(max_length=15, blank=True, null=True)
+    pin_code = models.CharField(max_length=6, blank=True, null=True)
+    latitude = models.CharField(max_length=20, blank=True, null=True)
+    longitude = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
--- food_stores/settings.py
+++ food_stores/settings.py
@@ -123,11 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'static'
 STATICFILES_DIRS = [
     'food_stores/static'
 ]
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
