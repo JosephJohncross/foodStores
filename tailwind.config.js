09ba27f8b7e9a64b36134b6d676090907eb65895
Joseph Ibochi <josephibochi@gmail.com>
2022-10-19 02:20:38 +0100

    minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,22 @@ module.exports = {
         'baskerville': ['"Libre Baskerville"', 'serif']
       },
       colors: {
-        'mainColor': '#eaa023',
+        "mainColor":"#FF7A00",
         'mainDark': '#0c1023',
         'mainDarkLight': '#191f3a',
         'whiteLight': '#c8c8c8',
       },
       boxShadow: {
         'shadow1': '0 0 15px rgba(0,0,0,0.25)'
+      },
+      keyframes: {
+        scale: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'rotate(1.3)' },
+        }
+      },
+      animation: {
+        'scale': 'scale 3s linear infinite',
       }
     },
   },
