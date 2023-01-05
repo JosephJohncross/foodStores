026870e8def2cedbf2a3d591fcf4be5cd28246b5
Joseph Ibochi <josephibochi@gmail.com>
2023-01-05 23:55:52 +0100

    Added vendor details page and implementaion in marketplace

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('marketplace', views.marketplace, name="marketplace")
+    path('marketplace', views.marketplace, name="marketplace"),
+    path('<slug:vendor_slug>/', views.vendor_details, name='vendor_details'),
+    path('<slug:vendor_slug>/menu/', views.vendor_menu, name="vendor_menu")
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from vendor.models import Vendor
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def marketplace(request):
@@ -10,3 +11,20 @@ def marketplace(request):
         'vendors_count': vendors_count
     }
     return render(request, 'marketplace/listing.html', context)
+
+def vendor_details(request, vendor_slug):
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    context = {
+        'vendor': vendor
+    }
+    return render(request, 'marketplace/vendor_details.html', context)
+
+def vendor_menu(request, vendor_slug):
+    slug = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+
+    context = {
+        'vendor_slug': slug,
+        'vendor': vendor
+    }
+    return render(request, 'marketplace/vendor_menu.html', context)
