37b8cde24511b34c6304d78c90481b39f817e2f8
Joseph Ibochi <josephibochi@gmail.com>
2023-01-06 23:28:16 +0100

    Added menu content for vendor details page

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from vendor.models import Vendor
 from django.shortcuts import get_object_or_404
+from menu.models import Category, FoodItem
+from django.db.models import Prefetch 
 
 # Create your views here.
 def marketplace(request):
@@ -22,9 +24,16 @@ def vendor_details(request, vendor_slug):
 def vendor_menu(request, vendor_slug):
     slug = get_object_or_404(Vendor, vendor_slug=vendor_slug)
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset = FoodItem.objects.filter(is_available=True)
+        )
+    )
 
     context = {
         'vendor_slug': slug,
-        'vendor': vendor
+        'vendor': vendor,
+        'categories': categories
     }
     return render(request, 'marketplace/vendor_menu.html', context)
