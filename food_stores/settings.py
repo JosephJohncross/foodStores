a64cfe8201c424f4d85ad416667a3b30fdcef268
Joseph Ibochi <josephibochi@gmail.com>
2023-02-22 21:59:37 +0100

    added build.sh file containing commands to run

--- food_stores/settings.py
+++ food_stores/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'livereload.middleware.LiveReloadScript',
