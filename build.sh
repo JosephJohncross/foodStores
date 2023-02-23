40e28f02a57517f9146fe3d1192f40d2460bf268
Joseph Ibochi <josephibochi@gmail.com>
2023-02-23 02:28:18 +0100

    changes to bash file

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 set -o errexit
 
 pip install -r requirements.txt
-pip install GDAL-3.4.3-cp311-cp311-win_amd64.whl, gunicorn
+
+sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+sudo apt-get update
+sudo apt-get install gdal-bin
+sudo apt-get install libgdal-dev
+export CPLUS_INCLUDE_PATH=/usr/include/gdal
+export C_INCLUDE_PATH=/usr/include/gdal
+pip install GDAL
 
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+sudo apt install gunicorn
--- food_stores/settings.py
+++ food_stores/settings.py
@@ -172,10 +172,10 @@ SECUREE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 GOOGLE_API_KEY = config('GOOGLE_API_KEY')
 FORWARD_GEOCODING = config('X_RAPID_API_KEY')
 
-
-os.environ['PATH'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
-os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
-GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\gdal304.dll')
+if DEBUG == True:
+    os.environ['PATH'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
+    os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
+    GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\gdal304.dll')
 
 # import sentry_sdk
 # from sentry_sdk.integrations.django import DjangoIntegration
