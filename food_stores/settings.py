0213a20170be5717dce7390338b9affa279c96d7
Joseph Ibochi <josephibochi@gmail.com>
2023-02-24 02:26:08 +0100

    modified static dir

--- food_stores/settings.py
+++ food_stores/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = './static'
+STATIC_ROOT = '/opt/render/project/src/static'
 # DEvelopment
 # STATICFILES_DIRS = [
 #     'food_stores/static',
